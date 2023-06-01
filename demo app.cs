fe84d9179def77b1c21f364ecc3ba31fca4f5ba6
Abimanyu10023 <133190170+Abimanyu10023@users.noreply.github.com>
2023-06-01 15:12:01 +0530

    Update demo app.cs

--- demo app.cs
+++ demo app.cs
@@ -1,55 +1,57 @@
-using System;
 using System.IO;
-using System.Net.Http;
-using System.Threading.Tasks;
+using Azure.Storage.Blobs;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
 
-namespace ResumeUploader
+namespace ResumeUploader.Controllers
 {
-    class Program
+    public class ResumeController : Controller
     {
-        static async Task Main(string[] args)
+        private readonly IConfiguration _configuration;
+        private readonly BlobServiceClient _blobServiceClient;
+        private readonly string _containerName;
+
+        public ResumeController(IConfiguration configuration)
         {
-            string resumeFilePath = "https://github.com/Abimanyu10023/dotnetapp.githttps://abistore.blob.core.windows.net/abi03-containerr"; // Update with the actual file path of your resume
+            _configuration = configuration;
+            string connectionString = _configuration.GetValue<string>("DefaultEndpointsProtocol=https;AccountName=abistore;AccountKey=H0s/4SnM9JQhnTOO/YwY8liTMv5ZdbBzwhnQhHsyfHRbr7ADvpv0OQVX7OBQww1/oYPG1s6rm7kr+ASt7LfL1g==;EndpointSuffix=core.windows.net");
+            _blobServiceClient = new BlobServiceClient(connectionString);
+            _containerName = _configuration.GetValue<string>("abi03-containerr");
+        }
 
-            try
+        [HttpGet]
+        public IActionResult Upload()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Upload(IFormFile file)
+        {
+            if (file != null && file.Length > 0)
             {
-                using (var httpClient = new HttpClient())
+                // Get a reference to the Blob container
+                BlobContainerClient containerClient = _blobServiceClient.GetBlobContainerClient(_containerName);
+
+                // Create a unique name for the resume file
+                string fileName = Path.GetFileName(file.FileName);
+                string uniqueFileName = Path.GetFileNameWithoutExtension(fileName) + "_" + Path.GetRandomFileName() + Path.GetExtension(fileName);
+
+                // Upload the resume file to the Blob container
+                using (Stream stream = file.OpenReadStream())
                 {
-                    // Read the resume file as bytes
-                    byte[] fileBytes = File.ReadAllBytes(resumeFilePath);
-
-                    // Create a new MultipartFormDataContent
-                    var multipartContent = new MultipartFormDataContent();
-
-                    // Convert the file bytes to a StreamContent
-                    var fileContent = new ByteArrayContent(fileBytes);
-                    fileContent.Headers.ContentDisposition = new System.Net.Http.Headers.ContentDispositionHeaderValue("form-data")
-                    {
-                        Name = "resume",
-                        FileName = Path.GetFileName(resumeFilePath)
-                    };
-
-                    // Add the file content to the MultipartFormDataContent
-                    multipartContent.Add(fileContent);
-
-                    // Send the HTTP POST request to the server
-                    var response = await httpClient.PostAsync("https://abistore.blob.core.windows.net/abi03-containerr", multipartContent);
-
-                    // Check if the upload was successful
-                    if (response.IsSuccessStatusCode)
-                    {
-                        Console.WriteLine("Resume uploaded successfully!");
-                    }
-                    else
-                    {
-                        Console.WriteLine("Resume upload failed. Status code: " + response.StatusCode);
-                    }
+                    containerClient.UploadBlob(uniqueFileName, stream);
                 }
+
+                ViewBag.Message = "Resume uploaded successfully!";
             }
-            catch (Exception ex)
+            else
             {
-                Console.WriteLine("An error occurred: " + ex.Message);
+                ViewBag.Message = "No file selected for upload.";
             }
+
+            return View();
         }
     }
 }
